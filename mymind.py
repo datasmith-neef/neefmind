577ebcfd713fe6965d211190f8b180532ac2af7c
Armin R. <66879529+datasmith-neef@users.noreply.github.com>
2025-02-06 18:05:00 +0100

    second change

--- mymind.py
+++ mymind.py
@@ -37,7 +37,7 @@ if "notes" not in st.session_state:
     st.session_state.notes = []
 
 # Anwendungstitel
-st.title("SmithMind Proof of Concept")
+st.title("SmithMind POC")
 
 # Notizerfassung
 st.header("Neue Notiz hinzufügen")
